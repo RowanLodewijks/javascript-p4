666909d5070a47e63e4adf03b85ed0d6290fba44
RowanLodewijks <90310170+RowanLodewijks@users.noreply.github.com>
2022-06-09 09:23:24 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,91 @@
+function runFunction() {
+}
+let textbox = document.querySelector(".start-screen");
+
+window.addEventListener("keydown", (e) => {
+  if (e.key) {
+    textbox.style.display = "none";
+    runFunction();
+  }
+});
+
+
+
 var score = 0;
 var scoreboard = document.querySelector(".score");
 scoreboard.innerHTML = score;
-function hit(value, id)
-{
+
+function hit(value, id) {
   score = score + value;
-  scoreboard.innerHTML = score; 
-  
+  scoreboard.innerHTML = score;
+
 }
-function moveDerps(){
-  for (a=1;a<4;a++)
-    {
-      document.querySelector("#derp"+a).style.top = Math.floor((Math.random() * 500) + 1) + "px";  
-      document.querySelector("#derp"+a).style.left = Math.floor((Math.random() * 1200) + 1) + "px"; 
-    }
+
+function moveDerps() {
+  for (a = 1; a < 5; a++) {
+    document.querySelector("#derp" + a).style.top = Math.floor((Math.random() * 500) + 1) + "px";
+    document.querySelector("#derp" + a).style.left = Math.floor((Math.random() * 1200) + 1) + "px";
+  }
 }
 setInterval(moveDerps, 2000);
+
+function moveDerp2() {
+  for (a = 1; a < 4; a++) {
+    document.querySelector("#derp" + 2).style.top = Math.floor((Math.random() * 500) + 1) + "px";
+    document.querySelector("#derp" + 2).style.left = Math.floor((Math.random() * 1200) + 1) + "px";
+  }
+}
+setInterval(moveDerp2, 2430);
+
+function moveDerp3() {
+  for (a = 1; a < 4; a++) {
+    document.querySelector("#derp" + 3).style.top = Math.floor((Math.random() * 500) + 1) + "px";
+    document.querySelector("#derp" + 3).style.left = Math.floor((Math.random() * 1200) + 1) + "px";
+  }
+}
+setInterval(moveDerp3, 1000);
+
+// function spawnGolden() {
+//   var img = document.getElementById('derp4');
+//   img.style.visibility = 'visible';
+// }
+
+// setInterval(spawnGolden, 5000);
+
+// function despawnGolden() {
+//   var img = document.getElementById('derp4');
+//   img.style.visibility = 'hidden';
+// }
+
+// setInterval(despawnGolden, 6000);
+
+
+
+function spawnGolden() {
+  var img = document.getElementById('derp4');
+  img.style.visibility = 'visible';
+
+  setInterval(() => {
+    var img = document.getElementById('derp4');
+    img.style.visibility = 'hidden';
+}, 1000);
+}
+
+setInterval(spawnGolden, 5000);
+
+
+
+
+// let c = 0;
+// setInterval(() => {
+//   c += 1;
+//   if (c === 2)
+//   var img = document.getElementById('derp4');
+//   img.style.visibility = 'visible';
+//   if (c === 4) 
+//   var img = document.getElementById('derp4');
+//   img.style.visibility = 'hidden';
+//   if (c === 5) c = 0;
+
+//   console.log(c);
+// }, 1000);
